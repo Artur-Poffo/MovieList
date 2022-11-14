c0d94194ed2da0c380026615475b4f69c0a28011
Artur Poffo <arturpoffoc@hotmail.com>
2022-11-14 04:00:06 -0300

    Component Image and styled-components config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,16 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+        port: "",
+        pathname: "/t/p/**"
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
--- src/components/MovieCard/index.jsx
+++ src/components/MovieCard/index.jsx
@@ -1,11 +1,14 @@
 import { Card } from "./styles"
 import Link from "next/link"
+import Image from "next/image"
 
 export default function MovieCard(props) {
+  const urlImage = `https://image.tmdb.org/t/p/original${props.poster}`
+
   return (
     <Card key={props.id} >
       <Link href={`/movie/${props.id}`} >
-        <img width={220} src={`https://image.tmdb.org/t/p/original${props.poster}`} alt="Imagem do filme" />
+        <Image width={220} height={300} placeholder="blur" blurDataURL={urlImage} src={urlImage} alt="Imagem do filme" />
         <p>{props.title}</p>
       </Link>
     </Card>
